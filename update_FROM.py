a86fbf74c6f2759f5e8904aa60a70c26ede3f8e3
HyeLynnKIM <helen6339@naver.com>
2023-01-02 21:37:51 +0900

    update - 01/02

--- update_FROM.py
+++ update_FROM.py
@@ -13,27 +13,27 @@ def Insert_FROM(sql: str):
     try: # 먼저 where 있을 때 확인
         text = str(re.match(p1, sql))
         text = text.split('match=\'')[1].split('\'')[0].split('select ')[1].split('where')[0]
-        sql = re.sub(p1, 'select ' + text + 'FROM [table] where', sql)
+        sql = re.sub(p1, 'select ' + text + 'FROM [TABLE] where', sql)
     except:
         try: # where X -> group by 확인
             text = str(re.match(p2, sql))
             text = text.split('match=\'')[1].split('\'')[0].split('select ')[1].split('group by')[0]
-            sql = re.sub(p2, 'select ' + text + 'FROM [table] where', sql)
+            sql = re.sub(p2, 'select ' + text + 'FROM [TABLE] where', sql)
         except:
             if re.match(p3, sql) != None: # group by X -> having 확인
                 text = str(re.match(p3, sql))
                 text = text.split('match=\'')[1].split('\'')[0].split('select ')[1].split('having')[0]
-                sql = re.sub(p3, 'select ' + text + 'FROM [table] where', sql)
+                sql = re.sub(p3, 'select ' + text + 'FROM [TABLE] where', sql)
             elif re.match(p4, sql) != None: # having X -> order by 확인
                 text = str(re.match(p4, sql))
                 text = text.split('match=\'')[1].split('\'')[0].split('select ')[1].split('order by')[0]
-                sql = re.sub(p4, 'select ' + text + 'FROM [table] where', sql)
+                sql = re.sub(p4, 'select ' + text + 'FROM [TABLE] where', sql)
             elif re.match(p5, sql) != None: # order by X -> limit 확인
                 text = str(re.match(p5, sql))
                 text = text.split('match=\'')[1].split('\'')[0].split('select ')[1].split('limit')[0]
-                sql = re.sub(p5, 'select ' + text + 'FROM [table] where', sql)
+                sql = re.sub(p5, 'select ' + text + 'FROM [TABLE] where', sql)
             else: # select만 있는 경우 (조건 X)
-                sql = sql + ' FROM [table]'
+                sql = sql + ' FROM [TABLE]'
 
     return sql
 
